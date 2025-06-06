7e60318799205ed641051082cfa9c11604278a0c
abul kalam <bd.abulkalam800@gamil.com>
2025-06-06 21:04:14 +0300

    Create homerepair server and mongodb

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+require('dotenv').config()
+const { ServerApiVersion, MongoClient } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 4000;
+
+
+
+console.log(process.env.DB_URI)
+const client = new MongoClient(process.env.DB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   //  await client.close();
+  }
+}
+run().catch(console.dir);
+ app.get('/',(req,res)=>{
+    
+    res.send("My server is runing")
+
+ })
+
+ app.listen(port,()=>{
+    console.log(`MY server is rouning this : ${port}`)
+ })
