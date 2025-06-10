b415c4f57ec810038656bfbdc1ce345b453f7fe0
abul kalam <bd.abulkalam800@gamil.com>
2025-06-10 15:54:59 +0300

    added delet api

--- index.js
+++ index.js
@@ -73,6 +73,14 @@ async function run() {
      res.send(result)
 
    })
+   //delet api
+   app.delete('/allservices/:id',async(req,res)=>{
+    const id =req.params.id;
+    const query= {_id: new ObjectId(id)}
+    const result = await collectionAllservices.deleteOne(query)
+    res.send(result)
+
+   })
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
